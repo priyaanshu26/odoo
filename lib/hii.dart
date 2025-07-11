ffbdf9719f77b0fdeb9804b472f9c28816b3afbc
Priyanshu Choudhary <priyanshu.171561@gmail.com>
2025-07-11 21:51:35 +0530

    feat: Create basic Hii widget
    This commit introduces a new Flutter widget named `Hii`.
    
    The `Hii` widget is a stateless widget that displays a `Scaffold` with a centered `Text` widget showing the string "Hii".

---
+++ lib/hii.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Hii extends StatelessWidget {
+  const Hii({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(
+        child: Text('Hii'),
+      ),
+    );
+  }
+}
