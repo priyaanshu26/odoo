03296170fd9165f4762a6ecdc28a3ec3f45534d8
RankjaVinit <vinupatel1982@gmail.com>
2025-07-11 21:52:16 +0530

    feat: Create basic Hii widget
    This commit introduces a new Flutter StatefulWidget named `Hii`.
    
    The widget consists of:
    - A `Scaffold` as the root element.
    - An `AppBar` with the title "Demo Page", centered.
    
    The purpose of this widget is to provide a basic page structure.

---
+++ lib/hii.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Hii extends StatefulWidget {
+  const Hii({super.key});
+
+  @override
+  State<Hii> createState() => _HiiState();
+}
+
+class _HiiState extends State<Hii> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Demo Page'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
