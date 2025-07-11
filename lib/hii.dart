453e396707faa3cf16745c6d67f9690fb84c4955
RankjaVinit <vinupatel1982@gmail.com>
2025-07-11 21:57:12 +0530

    feat: Create Hii StatefulWidget
    This commit introduces a new Flutter StatefulWidget named `Hii`.
    
    The `Hii` widget is initialized with a `key` parameter. Its corresponding state class, `_HiiState`, currently builds an empty `Scaffold`.

---
+++ lib/hii.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Hii extends StatefulWidget {
+  const Hii({super.key});
+
+  @override
+  State<Hii> createState() => _HiiState();
+}
+
+class _HiiState extends State<Hii> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
