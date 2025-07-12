67488029b69443e0f92d4aa9234ef04cc42b27c6
Priyanshu Choudhary <priyanshu.171561@gmail.com>
2025-07-12 14:22:45 +0530

    Remove RichTextEditor test file
    The file `lib/test/test_rich_text.dart` has been deleted. This file contained a simple `RichTextEditor` widget using `flutter_quill` for testing purposes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:odoo/screens/add_que/add_view.dart';
-import 'test/test_rich_text.dart'; // ✅ Make sure this path is correct
 
 void main() {
   runApp(const MyApp());
