50bfece06fb850422152057047fd8f6798146bf5
Priyanshu Choudhary <priyanshu.171561@gmail.com>
2025-07-12 14:26:30 +0530

    feat: Remove Flutter app initialization
    This commit removes the main application setup in `lib/main.dart`. The `MyApp` widget and the `runApp` call have been deleted, leaving an empty `main` function.
    
    Previously, `main.dart` initialized a `GetMaterialApp` with `AskQuestionScreen` as its home.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:odoo/screens/add_que/add_view.dart';
+void main(){
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AskQuestionScreen(), // ✅ This is now defined
-    );
-  }
 }
